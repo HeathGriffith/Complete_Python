7aed229266f20573d1d892361a8010f72141f708
Heath Griffith <heathgriff@gmail.com>
2024-02-14 05:57:06 -0600

    add exercise:  method,  function

---
+++ 01_Basics_01/07_sort.py
@@ -0,0 +1,6 @@
+#fix this code so that it prints a sorted list of all of our friends 
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
+
+new_friend = ['Stanley']
+friends.extend(new_friend)
+print(sorted(friends))
