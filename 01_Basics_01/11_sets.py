5d248c3c1f0a65ad2c4c367dd00acc8f25f6761f
Heath Griffith <heathgriff@gmail.com>
2024-02-15 13:18:38 -0600

    add notes: sets intro

---
+++ 01_Basics_01/11_sets.py
@@ -0,0 +1,12 @@
+my_list = [1,2,3,4,5,5]
+
+# return nonduplicate values
+print(set(my_list))
+
+my_set = {7,8,9,10,10}
+# Access items in set not by index like list:
+# i.e., not `print(my_set[0]),
+# but by the item, like a dictionary
+
+# check for existence
+print(7 in my_set)
