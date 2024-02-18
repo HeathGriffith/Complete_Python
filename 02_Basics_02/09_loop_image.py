31eeda4e717905e632bbea7f4cbb3f341c03d2ba
Heath Griffith <heathgriff@gmail.com>
2024-02-18 15:42:06 -0600

    add exercise: loop to output image

---
+++ 02_Basics_02/09_loop_image.py
@@ -0,0 +1,65 @@
+#Display the picture where the 0 is going to be ' ',
+#    and the 1 is going to be '*'. This will reveal an image!
+
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+
+# # first try
+# for row in picture:
+#     for pixel in row:
+#         if pixel == 0:
+#             print(' ')
+#         else:
+#             print('*')
+
+# # while-loop
+# row = 0
+# while row < len(picture):
+#     GUI = []
+#     for number in picture[row]:
+#         if number == 0:
+#             GUI.append(' ')
+#         else:
+#             GUI.append('*')
+#     print(GUI)
+#     row += 1
+
+# # for-loop
+
+# for row in picture:
+#     GUI = []
+#     for number in row:
+#         if number == 0:
+#             GUI.append(' ')
+#         else:
+#             GUI.append('*')
+#     print(GUI)
+
+# # *** fourth try: output string:
+
+# for row in picture:
+#     line = ''
+#     for pixel in row:
+#         if pixel == 0:
+#             line += ' '
+#         else:
+#             line += '*'
+#     print(line)
+
+# instructor solution
+for row in picture:
+    for pixel in row:
+        if (pixel == 1):
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print('')
+
+
+
