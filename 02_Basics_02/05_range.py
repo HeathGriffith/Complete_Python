299e7682ff73e75e2b5758cfc613cdb564d7e652
Heath Griffith <heathgriff@gmail.com>
2024-02-18 05:47:33 -0600

    update note: add  to range notes

--- 02_Basics_02/05_range.py
+++ 02_Basics_02/05_range.py
@@ -4,5 +4,28 @@
 # for number in range(1, 100):
 #     print(number)
 
-for number in range(0, 10):
-    print("Hi")
+# for _ in range(0, 10): # '_' variable indicates we don't care what the number is
+#     print("Hi")
+
+# for _ in range(0, 10, 2): # third parameter is a step over with default 1
+#     print(_)
+
+# for _ in range(10, 0, -1): # go in reverse
+#     print(_)
+
+# for _ in range(10, 0, -2): # loop five times
+#     print(list(range(10))) # convert range (literable item) into list
+
+# for char in enumerate('Hello'): # enumerate takes iterable object
+#     # gives index and item of index
+#     print(char)
+# # But this is less readable, and each tuple would need to be 
+#     # unpacked to access elements; 
+
+# # Hence, correct approach is usually to unpack upfront:
+# for i, char in enumerate('Hello'):
+#     print(i, char)
+
+for i, number in enumerate(list(range(100))):
+    if number == 50:
+        print(f'the index of 50 is {i}')
