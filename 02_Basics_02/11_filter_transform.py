167f475685b36cb6bb1ec421e35904ee27d13366
Heath Griffith <heathgriff@gmail.com>
2024-02-22 06:36:48 -0600

    add practice exercise: add GPT generated exercise

---
+++ 02_Basics_02/11_filter_transform.py
@@ -0,0 +1,19 @@
+# Filter out all the numbers from mixed_list and create a new list called numbers_list.
+# Filter out all the strings from mixed_list and create a new list called strings_list.
+# For each number in numbers_list, multiply it by 2.
+# For each string in strings_list, concatenate it with itself (e.g., 'a' becomes 'aa').
+# Print both modified lists.
+
+mixed_list = [1, 2, 'a', 'b', 3, 'c', 4, 'd', 'e', 5, 'f']
+
+numbers_list = []
+strings_list = []
+
+for item in mixed_list:
+    if type(item) == int:
+        numbers_list.append(item)
+    else: strings_list.append(item)
+
+print(numbers_list)
+# print(strings_list)
+
